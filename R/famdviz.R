ef1059b43035114b4523d5907361e130e1d59f73
petriv03 <154843713+petriv03@users.noreply.github.com>
2023-12-28 23:25:29 +0100

    modif

--- R/famdviz.R
+++ R/famdviz.R
@@ -1,31 +1,41 @@
 #' Plot variables
 #'
-#' Calculate the square root of a numeric value
-#' @param x the numeric value
-#' @return the square root of x
+#' Draw a scatter plot of qualitative and quantitative variables coordinated by association with individuals in FAMD model.
+#' @param famd list produced by factoMineR::FAMD function
+#' @param dim_x first dimension integer
+#' @param dim_y second dimension integer
+#' @param color_by "contrib" or "cos2" (see factoMineR::FAMD documentation)
+#' @param axis_text_x boolean indicating labels on horizontal axis tics
+#' @param axis_text_y boolean indicating labels on vertical axis tics
+#' @return scatter ggplot of variables colored by cos2 or contribution
 #' @examples
-#' sqroot(22);
+#' library(factoMineR)
+#' df <- as.data.frame(example)
+#' famd <- factoMineR::FAMD(df, graph = FALSE)
+#' p <- plot_variables(famd, 1, 2, "cos2", FALSE, TRUE)
+#' print(p)
 #' @export
-plot_variables <- function(famd, dim_x, dim_y, color_by) {
+plot_variables <- function(famd, dim_x, dim_y, color_by, axis_text_x,
+                           axis_text_y) {
   variables <- get_variables(famd, dim_x, dim_y, color_by)
   mean_color <- mean(variables[, 3])
-  figure <- ggplot2::ggplot(variables, ggplot2::aes(x = data_frame[, 1],
-                                                    y = data_frame[, 2],
-                                                    color = data_frame[, 3],
+  figure <- ggplot2::ggplot(variables, ggplot2::aes(x = variables[, 1],
+                                                    y = variables[, 2],
+                                                    color = variables[, 3],
                                                     label = rownames(variables)
                                                     )
                             ) +
     ggplot2::geom_hline(yintercept = 0, linetype = "dashed") +
     ggplot2::geom_vline(xintercept = 0, linetype = "dashed") +
     ggplot2::geom_point(shape = 17) +
-    ggrepel::geom_text_repel(max.overlaps = Inf, size=3) +
+    ggrepel::geom_text_repel(max.overlaps = Inf, size = 3) +
     ggplot2::labs(x = NULL, y = NULL) +
     set_gradient(mean_color) +
     set_custom_theme(legend_position_x = 1,
                      legend_position_y = 1,
-                     axis_text_x = T,
-                     axis_text_y = T) +
-    ggplot2::guides(color = guide_colourbar(title = toupper(color_by)))
+                     axis_text_x = axis_text_x,
+                     axis_text_y = axis_text_y) +
+    ggplot2::guides(color = ggplot2::guide_colourbar(title = toupper(color_by)))
   return(figure)
 }
 
@@ -55,6 +65,80 @@ get_variables <- function(famd, dim_x, dim_y, color_by) {
   variables <- cbind(coordinates, colors)
   return(variables)
 }
+
+#' Plot individuals
+#'
+#' Draw a scatter plot of individuals in FAMD model.
+#' @param famd list produced by factoMineR::FAMD function
+#' @param dim_x first dimension integer
+#' @param dim_y second dimension integer
+#' @param color_by column of original dataframe which a color comes from
+#' @param size_by column of original dataframe which a size comes from
+#' @param axis_text_x boolean indicating labels on horizontal axis tics
+#' @param axis_text_y boolean indicating labels on vertical axis tics
+#' @param ellipse boolean indicating t-distribution ellipse
+#' @return scatter ggplot of variables colored by cos2 or contribution
+#' @examples
+#' library(factoMineR)
+#' df <- as.data.frame(example)
+#' famd <- factoMineR::FAMD(df, graph = FALSE)
+#' p <- plot_variables(famd, 1, 2, "age", "income", FALSE, TRUE)
+#' print(p)
+#' @export
+plot_individuals <- function(famd, dim_x, dim_y,
+                             shape_by = NULL,
+                             color_by = NULL,
+                             size_by = NULL,
+                             axis_text_x = TRUE,
+                             axis_text_y = TRUE,
+                             ellipse = FALSE) {
+  individuals <- get_individuals(famd, dim_x, dim_y, color_by, size_by)
+  if ()
+  aes
+
+  figure <- ggplot2::ggplot(individuals, ggplot2::aes(x = individuals[, 1],
+                                                    y = individuals[, 2],
+                                                    color = variables[, 3],
+                                                    label = rownames(variables)
+  )
+  ) +
+    ggplot2::geom_hline(yintercept = 0, linetype = "dashed") +
+    ggplot2::geom_vline(xintercept = 0, linetype = "dashed") +
+    ggplot2::geom_point(shape = 17) +
+    ggrepel::geom_text_repel(max.overlaps = Inf, size = 3) +
+    ggplot2::labs(x = NULL, y = NULL) +
+    set_gradient(mean_color) +
+    set_custom_theme(legend_position_x = 1,
+                     legend_position_y = 1,
+                     axis_text_x = axis_text_x,
+                     axis_text_y = axis_text_y) +
+    ggplot2::guides(color = ggplot2::guide_colourbar(title = toupper(color_by)))
+  return(figure)
+}
+
+#' Get Individuals
+#'
+#' Get individuals of factoMineR::FAMD model as an input for plot_individuals function
+#' @param famd list produced by factoMineR::FAMD function
+#' @param dim_x first dimension integer
+#' @param dim_y second dimension integer
+#' @param color_by column of original dataframe which a color comes from
+#' @param size_by column of original dataframe which a size comes from
+#' @return a data frame of x, y coordinates and color/size intensities
+#' @examples
+#' famd <- factoMineR::FAMD(x, graph = F)
+#' inds <- get_individuals(famd, 1, 2, "cos2", "age", "income")
+#' print(inds)
+#' @export
+get_individuals <- function(famd, dim_x, dim_y, color_by, size_by) {
+  coordinates <- famd$ind$coord[, c(dim_x, dim_y)]
+  colors <- famd$call$X[, color_by]
+  sizes <- famd$call$X[, size_by]
+  individuals <- cbind(coordinates, colors, sizes)
+  return(individuals)
+}
+
+
 
 #' Set Gradient
 #'
